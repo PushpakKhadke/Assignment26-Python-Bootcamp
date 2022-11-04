747d66c7eac83472420ba1f359afd589ed9fe8aa
Pushpak Khadke <100704509+PushpakKhadke@users.noreply.github.com>
2022-11-04 16:31:20 +0530

    "Assignment 26"

---
+++ Assignemnt26.py
@@ -0,0 +1,21 @@
+# Assignment 26
+
+# 1. Write a python script to multiple 2 or 3 or 4 numbers with a single multiply method in a class using method overloading.
+
+# 2. Write a python script to create a user account class with 2 instance variables (userid and balance). Create 3 user objects and add all the users using operator overloading.
+
+# 3. Write a python script to create a to print the above user account object using operator overloading (hint __str__ method).
+
+# 4. Write a python script to create two Threads. First thread will print all Even numbers and Second thread will print all Odd numbers till 100.
+
+# 5. Write a python script to create 2 threads to add all the values from 1 to 100.
+
+# 6. Write a python script to create 5 threads to fill a list with random numbers till 100.
+
+# 7. Write a python script to create a clock where 1st thread will print the current time every second and 2nd will print “1 Minute Completed” after every 1 minute.
+
+# 8. Write a python script to change the name of the Thread.
+
+# 9. Write a python script to join 2 threads after printing completing the first Question.
+
+# 10. Write a python script to check whether a given number is prime or armstrong number using 2 different threads.
